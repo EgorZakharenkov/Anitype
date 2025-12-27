947e8aab387456e138c5bb9780fc87e92b0c13bb
Egor <egorboom2017@gmail.com>
2025-12-27 08:49:40 +0300

    update

--- components/shared/Search/index.tsx
+++ components/shared/Search/index.tsx
@@ -1,16 +1,13 @@
 "use client";
 
 import { FC, SetStateAction, useState } from "react";
-import { useOutside } from "@/lib/hooks/useOutside";
 import { Ellipse } from "@/components/ui/Ellipse";
 import { Input } from "@/components/ui/Input";
 import { SearchIcon } from "@/components/shared/icons/search";
 import {
   Dialog,
   DialogContent,
-  DialogDescription,
   DialogHeader,
-  DialogTitle,
   DialogTrigger,
 } from "@/components/ui/shadcn/dialog";
 
@@ -20,12 +17,6 @@ interface SearchProps {
 
 export const Search: FC<SearchProps> = ({}) => {
   const [value, setValue] = useState<string>("");
-  const [open, setOpen] = useState<boolean>(false);
-  const outsideClick = useOutside(() => setOpen(false));
-
-  const handleClick = () => {
-    setOpen(true);
-  };
 
   const handleChangeValue = (e: {
     target: { value: SetStateAction<string> };
@@ -42,11 +33,11 @@ export const Search: FC<SearchProps> = ({}) => {
         </DialogTrigger>
         <DialogContent className="sm:max-w-[1200px] h-11/12">
           <DialogHeader>
-            <DialogTitle>Edit profile</DialogTitle>
-            <DialogDescription>
-              Make changes to your profile here. Click save when you&apos;re
-              done.
-            </DialogDescription>
+            <Input
+              value={value}
+              placeholder={"Название аниме..."}
+              onChange={handleChangeValue}
+            />
           </DialogHeader>
         </DialogContent>
       </form>
